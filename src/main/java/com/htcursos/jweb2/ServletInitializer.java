1d19ce9955120e9ff84e6856dc7e73278176b82c
Renato Yuzo <renatoyuzo93@gmail.com>
2018-05-08 21:12:46 -0400

    Primeiro Commit

---
+++ src/main/java/com/htcursos/jweb2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.htcursos.jweb2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jweb2Application.class);
+	}
+
+}
